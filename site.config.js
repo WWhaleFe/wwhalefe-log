2db5fe1eb7ec11aea3c055c21f5fa98bc8d5a730
WWhaleFe <86756235+WWhaleFe@users.noreply.github.com>
2026-04-14 19:35:53 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `WWhaleFe-log`,
+      href: "https://github.com/WWhaleFe/WWhaleFe-log",
     },
   ],
   // blog setting (required)
